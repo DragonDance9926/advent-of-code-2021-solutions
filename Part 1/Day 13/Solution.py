25098b3ecc8bf95dc78b2a958487e30b90dcbf16
DragonDance9926 <batuhan_tanriverdi@outlook.com>
2021-12-13 10:20:57 +0300

    Day 13 (Only Part 1)
    I also solved the second part but rendering the letters was impossible because of my first part solution.

---
+++ Part 1/Day 13/Solution.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+with open("input.txt") as f:
+    a = f.read().split("\n\n")
+    coordinates = a[0]
+    coordinates = coordinates.split("\n")
+    folds = a[1]
+    folds = folds.split("\n")
+    
+dots = np.zeros((2000,2000),dtype=int)
+
+for s in coordinates:
+    a = s.split(",")
+    i = int(a[0])
+    j = int(a[1])
+    dots[j][i] = 1
+
+fold = folds[0]
+
+fold = fold.split()
+folding = fold[-1]
+folding = folding.split("=")
+coor = int(folding[-1])
+axis = folding[0]
+if axis == 'x': #j ()
+    x = coor + 1
+    for s in coordinates:
+        a = s.split(",")
+        i = int(a[0])
+        j = int(a[1])
+        if dots[j][i] == 1 and i > x:
+            dots[j][2*x-1] = 1
+            dots[j][i] = 0
+elif axis == 'y': #i
+    y = coor + 1
+    for s in coordinates:
+        a = s.split(",")
+        j = int(a[0])
+        i = int(a[1])
+        if dots[j][i] == 1 and j > y:
+            dots[2*y-1][i] = 1
+            dots[j][i] = 0
+            
+
+print(np.count_nonzero(dots))           
